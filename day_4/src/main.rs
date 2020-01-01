5cb6debe7b3dbe4c28e297066c9e981a629c0cbb
szopqa96@gmail.com <szopqa96@gmail.com>
2020-01-02 00:14:04 +0100

    DAY 4 part 1

--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -17,14 +17,35 @@ impl PasswordCracker {
     for each_char in possible_pass.chars() {
       iteration += 1;
       let next_elem = &possible_pass[iteration..if iteration == possible_pass.len() {iteration} else {iteration + 1}];
-      let first_elem: usize = 0;
 
-      println!("{} {}", each_char, next_elem);
       if each_char.to_string() == next_elem {
         at_least_one = true;
+        break;
       }
     }
     at_least_one
+  }
+
+  pub fn digits_dont_decrease(possible_pass: &String) -> bool {
+    let mut iteration = 0;
+    let mut digits_dont_decrease = false;
+    for each_char in possible_pass.chars() {
+      iteration += 1;
+      if (iteration == possible_pass.len()) {
+        break;
+      }
+
+      let next_elem = &possible_pass[iteration..iteration + 1];
+
+      if each_char.to_digit(10).unwrap() > next_elem.chars().map(|c| c.to_digit(10).unwrap()).sum::<u32>() {
+        digits_dont_decrease = false;
+        break;
+      } else {
+        digits_dont_decrease = true;
+      }
+    }
+
+    digits_dont_decrease 
   }
 
   pub fn crack(&self) -> i32 {
@@ -33,12 +54,15 @@ impl PasswordCracker {
     for each_possibility in self.pass_lower_range..self.pass_upper_limit {
       let each_possibility_str = each_possibility.to_string();
 
-      if Self::has_two_same_digits(&each_possibility_str) {
-        password_possibilities.push(each_possibility_str);
-      }
-      else {
+      if !Self::has_two_same_digits(&each_possibility_str) {
         continue;
       }
+
+      if !Self::digits_dont_decrease(&each_possibility_str) {
+        continue;
+      }
+
+      password_possibilities.push(each_possibility_str);
     }
 
     password_possibilities.len() as i32
@@ -46,7 +70,7 @@ impl PasswordCracker {
 }
 
 fn main() {
-  // const puzzle_in: (u32, u32) = (109165, 109166);
+  // const puzzle_in: (u32, u32) = (114560, 114561);
   const puzzle_in: (u32, u32) = (109165, 576723);
   let mut pass_cracker = PasswordCracker::new(puzzle_in);
 

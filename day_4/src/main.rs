61bbf83f4bc8224e4464680b2a5dbe021a804bca
szopqa96@gmail.com <szopqa96@gmail.com>
2020-01-04 01:09:47 +0100

    Added solution for part 2

--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -1,4 +1,3 @@
-#[derive(Debug)]
 struct PasswordCracker {
   pass_lower_range: u32,
   pass_upper_limit: u32,
@@ -26,26 +25,57 @@ impl PasswordCracker {
     at_least_one
   }
 
-  pub fn digits_dont_decrease(possible_pass: &String) -> bool {
+  pub fn digits_do_not_decrease(possible_pass: &String) -> bool {
     let mut iteration = 0;
-    let mut digits_dont_decrease = false;
+    let mut digits_do_not_decrease = false;
     for each_char in possible_pass.chars() {
       iteration += 1;
-      if (iteration == possible_pass.len()) {
+      if iteration == possible_pass.len() {
         break;
       }
 
       let next_elem = &possible_pass[iteration..iteration + 1];
 
       if each_char.to_digit(10).unwrap() > next_elem.chars().map(|c| c.to_digit(10).unwrap()).sum::<u32>() {
-        digits_dont_decrease = false;
+        digits_do_not_decrease = false;
         break;
       } else {
-        digits_dont_decrease = true;
+        digits_do_not_decrease = true;
       }
     }
 
-    digits_dont_decrease 
+    digits_do_not_decrease 
+  }
+
+  pub fn two_same_digits_do_not_repeat(possible_pass: &String) -> bool {
+    let mut iteration = 0;
+
+    let mut single_pair_exists = false;
+    for each_char in possible_pass.chars() {
+      iteration += 1;
+      let next_elem = &possible_pass[iteration..if iteration == possible_pass.len() {iteration} else {iteration + 1}];
+
+      let current_elem = each_char.to_string();
+      if current_elem == next_elem {
+        if iteration == 1 && &possible_pass[2..3] != next_elem { // unique pair exists as first two numbers
+          single_pair_exists = true;
+          break;
+        }
+        else if iteration == (possible_pass.len() - 1) { 
+          if current_elem != &possible_pass[iteration - 2..iteration - 1] { // unique pair exists as last two numbers
+            single_pair_exists = true;
+          }
+          break;
+        }
+
+        if current_elem != &possible_pass[iteration + 1..iteration + 2] && current_elem != &possible_pass[iteration - 2..iteration - 1]{ // unique pair exists in the middle
+          single_pair_exists = true;
+          break;
+        }
+      }
+    }
+
+    single_pair_exists
   }
 
   pub fn crack(&self) -> i32 {
@@ -58,7 +88,11 @@ impl PasswordCracker {
         continue;
       }
 
-      if !Self::digits_dont_decrease(&each_possibility_str) {
+      if !Self::digits_do_not_decrease(&each_possibility_str) {
+        continue;
+      }
+
+      if !Self::two_same_digits_do_not_repeat(&each_possibility_str) {
         continue;
       }
 
@@ -70,7 +104,6 @@ impl PasswordCracker {
 }
 
 fn main() {
-  // const puzzle_in: (u32, u32) = (114560, 114561);
   const puzzle_in: (u32, u32) = (109165, 576723);
   let mut pass_cracker = PasswordCracker::new(puzzle_in);
 

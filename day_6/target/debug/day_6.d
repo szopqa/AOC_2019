9b82ebb7bba1c3686277f034c999dea14dfb7a66
szopqa96@gmail.com <szopqa96@gmail.com>
2020-02-05 23:12:28 +0100

    Initial structure setup for day_6 solution

--- day_6/target/debug/day_6.d
+++
@@ -1,1 +0,0 @@
-/home/mszopa/Desktop/rust/advent_of_code/day_6/target/debug/day_6: /home/mszopa/Desktop/rust/advent_of_code/day_6/src/main.rs
--- day_6/target/debug/deps/day_6-bb22e132c35a067d.d
+++
@@ -1,5 +0,0 @@
-/home/mszopa/Desktop/rust/advent_of_code/day_6/target/debug/deps/day_6-bb22e132c35a067d: src/main.rs
-
-/home/mszopa/Desktop/rust/advent_of_code/day_6/target/debug/deps/day_6-bb22e132c35a067d.d: src/main.rs
-
-src/main.rs:

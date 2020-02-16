664a0db6d5d5d4d5f4ba8b7f64e4f9772e875d09
szopqa96@gmail.com <szopqa96@gmail.com>
2020-02-17 00:34:06 +0100

    fixed untracked files

--- day_7/target/debug/day_7.d
+++
@@ -1,1 +0,0 @@
-/home/mszopa/Desktop/rust/advent_of_code/day_7/target/debug/day_7: /home/mszopa/Desktop/rust/advent_of_code/day_7/src/Computer.rs /home/mszopa/Desktop/rust/advent_of_code/day_7/src/main.rs
--- day_7/target/debug/deps/day_7-835eaf053cf0bed7.d
+++
@@ -1,6 +0,0 @@
-/home/mszopa/Desktop/rust/advent_of_code/day_7/target/debug/deps/day_7-835eaf053cf0bed7: src/main.rs src/Computer.rs
-
-/home/mszopa/Desktop/rust/advent_of_code/day_7/target/debug/deps/day_7-835eaf053cf0bed7.d: src/main.rs src/Computer.rs
-
-src/main.rs:
-src/Computer.rs:

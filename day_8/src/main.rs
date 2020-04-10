49dd914281c6b5c39b2d89c6a721094d7bf3e60f
szopqa <szopqa96@gmail.com>
2020-04-10 19:49:32 +0200

    Part one solution

--- day_8/src/main.rs
+++ day_8/src/main.rs
@@ -19,6 +19,18 @@ struct Layer <'a> {
     _elements: &'a str
 }
 
+impl <'a> Layer <'a> { 
+    fn get_num_of_occurrences(&self, _elem: char) -> i32 {
+        let mut _out = 0;
+        for _each_elem in self._elements.chars() {
+            if _each_elem == _elem {
+                _out += 1;
+            }
+        }
+        _out
+    }
+}
+
 fn main() {
     let _filename: String = String::from("input.txt");
     let _input = read_file_content(_filename);
@@ -26,14 +38,28 @@ fn main() {
 
     let _pixels_in_layer: i32 = IMAGE_HIGHT * IMAGE_WIDTH;
     let _num_of_layers: i32 = _elements / _pixels_in_layer;
-        
+
     let mut _layers: Vec<Layer> = vec![];
     for _layer_num in 0.._num_of_layers {
-        let _lower_bound = (_layer_num * _num_of_layers) as usize;
-        let _upper_bound = ((_layer_num + 1) * _num_of_layers - 1) as usize;
+        let _lower_bound = (_layer_num * _pixels_in_layer) as usize;
+        let _upper_bound = ((_layer_num + 1) * _pixels_in_layer) as usize;
 
         _layers.push(Layer {
             _elements: &_input[_lower_bound.._upper_bound]
         })
     }
+
+    let mut _min_zeros_occurrences_num = <i32>::max_value();
+    let mut _layer_with_min_zeros: Layer = Layer{ _elements: "" }; 
+    for _each_layer in _layers {
+        let _min_zeros_occurrences_in_layer = _each_layer.get_num_of_occurrences('0');
+
+        if _min_zeros_occurrences_in_layer < _min_zeros_occurrences_num {
+            _layer_with_min_zeros = _each_layer;
+            _min_zeros_occurrences_num = _min_zeros_occurrences_in_layer;
+        }
+    }
+
+    let _part_one_result = _layer_with_min_zeros.get_num_of_occurrences('1') * _layer_with_min_zeros.get_num_of_occurrences('2');
+    println!("Part one solution: {}", _part_one_result);
 }
